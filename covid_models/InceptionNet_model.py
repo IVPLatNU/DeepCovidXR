fab47ab154e717ad65555a26f2769d6a97f222e1
jsheng7 <39497817+jsheng7@users.noreply.github.com>
2020-07-08 17:08:03 -0500

    added InceptionNet model

---
+++ covid_models/InceptionNet_model.py
@@ -0,0 +1,34 @@
+# Build InceptionNet model
+
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+from tensorflow.keras.applications import InceptionV3
+
+
+class InceptionNet():
+    def __init__(self, weights):
+        self.weights = weights
+        
+    def build_model(self, img_size, dropout_rate):
+        base_model = InceptionV3(weights=self.weights, include_top=False, 
+                         input_shape = (img_size,img_size,3))
+        x = base_model.output
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dropout(dropout_rate)(x)
+        predictions = layers.Dense(1, activation='sigmoid', name='last')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        return model
+    
+    def freeze(self, model):
+        for layer in model.layers[:310]:
+            layer.trainable = False
+        for layer in model.layers[310:]:
+            layer.trainable = True
+            
+        return model
+    
+    def unfreeze(self, model):
+        for layer in model.layers[0:]:
+            layer.trainable = True
+        return model
+
