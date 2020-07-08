73631cc7aef9b471aa69d14aaf8fd4d23536d1c5
jsheng7 <39497817+jsheng7@users.noreply.github.com>
2020-07-08 15:25:04 -0500

    added dropour layers

--- covid_models/EfficientNet_model.py
+++ covid_models/EfficientNet_model.py
@@ -10,13 +10,14 @@ class EfficientNet():
     def __init__(self, weights):
         self.weights = weights
         
-    def build_model(self, img_size):
+    def build_model(self, img_size, dropout_rate):
         base_model = efn.EfficientNetB2(weights=self.weights, include_top=False, 
                                         backend = keras.backend, layers = keras.layers, 
                                         models = keras.models, utils = keras.utils,
                                         input_shape = (img_size,img_size,3))
         x = base_model.output
         x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dropout(dropout_rate)(x)
         predictions = layers.Dense(1, activation='sigmoid', name='last')(x)
         model = Model(inputs=base_model.input, outputs=predictions)
         return model
--- covid_models/Resnet_model.py
+++ covid_models/Resnet_model.py
@@ -9,11 +9,12 @@ class ResNet():
     def __init__(self, weights):
         self.weights = weights
         
-    def build_model(self, img_size):
+    def build_model(self, img_size, dropout_rate):
         base_model = ResNet50(weights=self.weights, include_top=False, 
                                  input_shape = (img_size,img_size,3))
         x = base_model.output
         x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dropout(dropout_rate)(x)
         predictions = layers.Dense(1, activation='sigmoid', name='last')(x)
         model = Model(inputs=base_model.input, outputs=predictions)
         return model
