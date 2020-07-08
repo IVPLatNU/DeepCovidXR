02c134526c423527852d188ac8048ec4b09721e5
jsheng7 <39497817+jsheng7@users.noreply.github.com>
2020-07-08 17:08:03 -0500

    added InceptionNet model

--- covid_models/__init__.py
+++ covid_models/__init__.py
@@ -2,3 +2,4 @@ from .xception_model import XceptionNet
 from .resnet_model import ResNet
 from .efficientNet_model import EfficientNet
 from .densenet_model import DenseNet
+from .inceptionnet_model import InceptionNet
