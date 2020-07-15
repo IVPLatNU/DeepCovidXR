42d07fc2379f4bf0f14387de906c8b1a5400e914
jsheng7 <39497817+jsheng7@users.noreply.github.com>
2020-07-14 21:04:36 -0500

    added train.py

--- pretrain.py
+++ pretrain.py
@@ -1,108 +1,112 @@
 # Pretrain a given model on NIH dataset
 import argparse
-
+import os
 from utils import nihUtils, imgUtils, trainFeatures
 from covid_models import DenseNet, ResNet, XceptionNet, EfficientNet, InceptionNet, InceptionResNet 
 
-# Implement command line argument
-parser = argparse.ArgumentParser(description='Pretrain a model on NIH dataset.')
-parser.add_argument('-m', '--model', dest = 'model_name')
-
-parser.add_argument('model', metavar = 'model_name', 
-                    choices = ['ResNet-50', 'Xception', 'DenseNet-121', 'Inception-V3', 
-                               'Inception-ResNet-V2', 'EfficientNet-B2'],
-                    type = str, nargs = 1,
-                    help = 'the name of the model to be trained with NIH dataset.\n Choose from ResNet-50, Xception, DenseNet-121, Inception-V3,' 
-                            'Inception-ResNet-V2, EfficientNet-B2')
-
-parser.add_argument('-s', '--size', dest = 'img_size')
-
-parser.add_argument('size', metavar = 'img_size',
-                    type = int, nargs = 1,
-                    help = 'the size of NIH images')
-
-parser.add_argument('path', metavar='NIH_path', type=str, nargs=1,
-                    help='the path that contains NIH dataset and NIH csv file or the path in which a new directory for NIH dataset will be created.')
-
-parser.add_argument('-c', '--create', dest = 'create_dir', nargs = '?', const = True,
-                    default = False,
-                    help = 'create a new directory for NIH dataset download')
-args = parser.parse_args()
-create_dir  = args.create_dir
-nih_path = args.path[0]
-model_name = args.model_name
-img_size = args.img_size
-batch_size = 16
-rotation_range = 20
-height_shift = 0.05
-width_shift = 0.05
-
-model_save_path = 'nih_weight_{name}.h5'.format(name = model_name)
-
-nih = nihUtils()
-if create_dir:
-    nih_path = nih.createDir(nih_path)
-    nih.nihDownload(nih_path)
-
-nih_img_path = nih_path
-nih_csv_path = nih_path
-csv_name = nih_csv_path + '\\NIH_Data_Entry.csv'
-
-train_df, val_df, labels = nih.nihSplit(csv_name, nih_img_path)
-
-img_proc = imgUtils(img_size)
-train_idg, val_idg = img_proc.dataGen(rotation_range, height_shift, width_shift)
-
-train_generator, val_generator = nih.nihGenerator(img_size, 
-                                                  batch_size, train_idg, val_idg, 
-                                                  train_df, val_df, labels)
-# Train a given model on NIH dataset
-lr = 0.001
-momentum = 0.9
-nestrov = True
-
-patience_rlr = 2
-patience_es = 10
-factor = 0.1
-min_delta = 0.001
-monitor = 'val_auc'
-
-if model_name == 'ResNet-50':
-    resnet = ResNet('imagenet')
-    model = resnet.buildBaseModel(img_size)
-    resnet.compileModel(model, lr, momentum, nestrov)
+def get_args():
+    # Implement command line argument
+    parser = argparse.ArgumentParser(description='Pretrain a model on NIH dataset.')
     
-elif model_name == 'Xception':
-    xception = XceptionNet('imagenet')
-    model = xception.buildBaseModel(img_size)
-    xception.compileModel(model, lr, momentum, nestrov)
-elif model_name == 'DenseNet-121':
-    dense = DenseNet('imagenet')
-    model = dense.buildBaseModel(img_size)
-    dense.compileModel(model, lr, momentum, nestrov)
+    parser.add_argument('-m', '-model', dest='model_name', metavar = 'model_name', 
+                        choices = ['ResNet-50', 'Xception', 'DenseNet-121', 'Inception-V3', 
+                                   'Inception-ResNet-V2', 'EfficientNet-B2'],
+                        type = str, nargs = 1, required = True, 
+                        help = 'the name of the model to be trained with NIH dataset.\n Choose from ResNet-50, Xception, DenseNet-121, Inception-V3,' 
+                                'Inception-ResNet-V2, EfficientNet-B2.')
     
-elif model_name == 'Inception-V3':
-    inception = InceptionNet('imagenet')
-    model = inception.buildBaseModel(img_size)
-    inception.compileModel(model, lr, momentum, nestrov)
+    parser.add_argument('-s', '--size', dest='img_size', metavar = 'img_size',
+                        type = int, nargs = 1, required = True,
+                        help = 'the size of NIH images')
     
-elif model_name == 'Inception-ResNet-V2':
-    inceptionres = InceptionResNet('imagenet')
-    model = inceptionres.buildBaseModel(img_size)
-    inceptionres.compileModel(model, lr, momentum, nestrov)
+    parser.add_argument('-p', '--path', dest='path', metavar='NIH_path', type=str, nargs=1, default = '',
+                        required = True, 
+                        help='the path that contains NIH dataset and NIH csv file or the path in which a new directory for NIH dataset will be created.')
+    return parser.parse_args()
     
-elif model_name == 'EfficientNet-B2':
-    efficient = EfficientNet('imagenet')
-    model = efficient.buildBaseModel(img_size)
-    efficient.compileModel(model, lr, momentum, nestrov)
 
-features = trainFeatures()
-rlp = features.setRLP(monitor, factor, patience_rlr)
-es = features.setES(monitor, patience_es, min_delta)
-cp = features.setCP(monitor, model_save_path)
+def get_model(model_name):
+    if model_name == 'ResNet-50':
+        resnet = ResNet('imagenet')
+        model = resnet.buildBaseModel(img_size)
+        resnet.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Xception':
+        xception = XceptionNet('imagenet')
+        model = xception.buildBaseModel(img_size)
+        xception.compileModel(model, lr, momentum, nestrov)
+    elif model_name == 'DenseNet-121':
+        dense = DenseNet('imagenet')
+        model = dense.buildBaseModel(img_size)
+        dense.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Inception-V3':
+        inception = InceptionNet('imagenet')
+        model = inception.buildBaseModel(img_size)
+        inception.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Inception-ResNet-V2':
+        inceptionres = InceptionResNet('imagenet')
+        model = inceptionres.buildBaseModel(img_size)
+        inceptionres.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'EfficientNet-B2':
+        efficient = EfficientNet('imagenet')
+        model = efficient.buildBaseModel(img_size)
+        efficient.compileModel(model, lr, momentum, nestrov)
+    return model
 
-epochs = 50
-features.NIHgenerator(model, batch_size, train_generator, val_generator, epochs, cp, rlp, es)
+if __name__=='__main__':
+
+    args = get_args()
+    nih_path = args.path[0]
+    img_size = args.img_size
+    model_name = args.model_name
+
+    batch_size = 16
+    rotation_range = 20
+    height_shift = 0.05
+    width_shift = 0.05
+
+    nih = nihUtils()
+    nih_path, create_dir = nih.createDir(nih_path)
+    model_save_path = os.path.join(nih_path, 'nih_weight_{name}.h5'.format(name = model_name))
+    
+    if create_dir:
+        nih.nihDownload(nih_path)
+    nih_img_path = nih_path
+    nih_csv_path = nih_path
+    csv_name = os.path.join(nih_csv_path, 'NIH_Data_Entry.csv')
+
+    train_df, val_df, labels = nih.nihSplit(csv_name, nih_img_path)
+    
+    img_proc = imgUtils(img_size)
+    train_idg, val_idg = img_proc.dataGen(rotation_range, height_shift, width_shift)
+    
+    train_generator, val_generator = nih.nihGenerator(img_size, 
+                                                      batch_size, train_idg, val_idg, 
+                                                      train_df, val_df, labels)
+    
+    model = get_model(model_name)
+    
+    # Train a given model on NIH dataset
+    lr = 0.001
+    momentum = 0.9
+    nestrov = True
+    
+    patience_rlr = 2
+    patience_es = 10
+    factor = 0.1
+    min_delta = 0.001
+    monitor = 'val_auc'
+    
+    features = trainFeatures()
+    rlp = features.setRLP(monitor, factor, patience_rlr)
+    es = features.setES(monitor, patience_es, min_delta)
+    cp = features.setCP(monitor, model_save_path)
+    
+    epochs = 50
+    features.NIHgenerator(model, batch_size, train_generator, val_generator, epochs, cp, rlp, es)
 
 
 
--- tuner.py
+++ tuner.py
@@ -6,173 +6,190 @@ from covid_models import hyperModel, DenseNet, ResNet, XceptionNet, EfficientNet
 import kerastuner
 from kerastuner.tuners import BayesianOptimization
 
-# Implement command line argument
-parser = argparse.ArgumentParser(description='Use keras tuner to find best hyper parameter.')
-parser.add_argument('-m', '--model', dest = 'model_name')
-
-parser.add_argument('model', metavar = 'model_name', 
-                    choices = ['ResNet-50', 'Xception', 'DenseNet-121', 'Inception-V3', 
-                               'Inception-ResNet-V2', 'EfficientNet-B2'],
-                    type = str, nargs = 1,
-                    help = 'the name of the model to be trained.\n Choose from ResNet-50, Xception, DenseNet-121, Inception-V3,' 
-                            'Inception-ResNet-V2, EfficientNet-B2')
-
-parser.add_argument('-s', '--size', dest = 'img_size')
-
-parser.add_argument('size', metavar = 'img_size',
-                    type = int, nargs = 1,
-                    help = 'the size of dataset images')
-
-parser.add_argument('path', metavar='DATA_path', type=str, nargs=1,
-                    help='the path that contains the dataset.')
-
-args = parser.parse_args()
-data_path = args.path[0]
-model_name = args.model_name
-img_size = args.img_size
-
-train_path = data_path + '\\Train'
-valid_path = data_path + '\\Validation'
-nih_weight = 'nih_weights_{name}.h5'.format(name = model_name)
-
-if not os.path.exists(nih_weight):
-    print('NIH weight does not exists.'
-          ' Please provide a NIH weight file in the format of nih_weight_[model name].h5')
-    exit()
+def get_args():
+    # Implement command line argument
+    parser = argparse.ArgumentParser(description='Use keras tuner to find best hyper parameter.')
     
-exp_name = 'tuner_pretrain'
-base_folder = os.getcwd()
-
-if not os.path.exists(base_folder + 'initial_cps/{}'.format(exp_name)):
-    os.makedirs(base_folder + 'initial_cps/{}'.format(exp_name))
+    parser.add_argument('-m', '--model', dest='model_name', metavar = 'model_name', 
+                        choices = ['ResNet-50', 'Xception', 'DenseNet-121', 'Inception-V3', 
+                                   'Inception-ResNet-V2', 'EfficientNet-B2'],
+                        type = str, nargs = 1, required = True, 
+                        help = 'the name of the model to be trained.\n Choose from ResNet-50, Xception, DenseNet-121, Inception-V3,' 
+                                'Inception-ResNet-V2, EfficientNet-B2')
     
-if not os.path.exists(base_folder + 'cps/{}'.format(exp_name)):
-    os.makedirs(base_folder + 'cps/{}'.format(exp_name))
-
-if not os.path.exists(base_folder + 'best_models_and_params/{}/model'.format(exp_name)):
-    os.makedirs(base_folder + 'best_models_and_params/{}/model'.format(exp_name))
-
-freeze_save_path = base_folder + 'initial_cps/{}.h5'.format(model_name)
-unfreeze_save_path = base_folder + 'cps/{}'.format(exp_name)
-best_model_path = base_folder + 'best_models_and_params/{}/model'.format(exp_name)
-best_weight_path = base_folder + 'best_models_and_params/{}/model_weights.h5'.format(exp_name)
-best_param_path = base_folder + 'best_models_and_params/{}/model_params'.format(exp_name)
-
-batch_size = 16
-rotation_range = 20
-height_shift = 0.05
-width_shift = 0.05
-
-img_proc = imgUtils(img_size)
-train_idg, val_idg = img_proc.dataGen(rotation_range, height_shift, width_shift)
-train_gen, val_gen = img_proc.generator(batch_size, train_idg, val_idg, train_path, valid_path)
-
-lr = 0.001
-momentum = 0.9
-nestrov = True
-
-# Train with only pooling layers
-
-if model_name == 'ResNet-50':
-    resnet = ResNet(nih_weight)
-    base = resnet.buildTunerModel(img_size)
-    model = resnet.buildBaseModel(img_size)
-    model = resnet.freeze(model)
-    resnet.compileModel(model, lr, momentum, nestrov)
+    parser.add_argument('--size', '-s', dest='img_size', metavar = 'img_size',
+                        type = int, nargs = 1, required = True, 
+                        help = 'the size of dataset images')
     
-elif model_name == 'Xception':
-    xception = XceptionNet(nih_weight)
-    base = xception.buildTunerModel(img_size)
-    model = xception.buildBaseModel(img_size)
-    model = xception.freeze(model)
-    xception.compileModel(model, lr, momentum, nestrov)
+    parser.add_argument('--path', '-p', dest='path', metavar='DATA_path', type=str, nargs=1,
+                        required = True, help='the path that contains the dataset.')
+    return parser.parse_args()
 
-elif model_name == 'DenseNet-121':
-    dense = DenseNet(nih_weight)
-    base = dense.buildTunerModel(img_size)
-    model = dense.buildBaseModel(img_size)
-    model = dense.freeze(model)
-    dense.compileModel(model, lr, momentum, nestrov)
+def make_path(data_dir, base, exp_name):
+    train_path = os.path.join(data_dir, 'Train')
+    valid_path = os.path.join(data_dir, 'Validation')
     
-elif model_name == 'Inception-V3':
-    inception = InceptionNet(nih_weight)
-    base = inception.buildTunerModel(img_size)
-    model = inception.buildBaseModel(img_size)
-    model = inception.freeze(model)
-    inception.compileModel(model, lr, momentum, nestrov)
+    if (not os.path.isdir(train_path)) or (not os.path.isdir(valid_path)):
+        print('Please split images into train directory and validation directory.')
+        exit()
+        
+    freeze_save_path = os.path.join(base, 'initial_cps/{}.h5'.format(model_name))
+    unfreeze_save_path = os.path.join(base, 'cps/{}'.format(exp_name))
+    best_model_path = os.path.join(base, 'best_models_and_params/{}/model'.format(exp_name))
+    best_weight_path = os.path.join(base, 'best_models_and_params/{}/model_weights.h5'.format(exp_name))
+    best_param_path = os.path.join(base, 'best_models_and_params/{}/model_params'.format(exp_name))
     
-elif model_name == 'Inception-ResNet-V2':
-    inceptionres = InceptionResNet(nih_weight)
-    base = inceptionres.buildTunerModel(img_size)
-    model = inceptionres.buildBaseModel(img_size)
-    model = inceptionres.freeze(model)
-    inceptionres.compileModel(model, lr, momentum, nestrov)
+    if not os.path.exists(os.path.join(base, 'initial_cps/{}'.format(exp_name))):
+        os.makedirs(os.path.join(base, 'initial_cps/{}'.format(exp_name)))
+        
+    if not os.path.exists(os.path.join(base, 'cps/{}'.format(exp_name))):
+        os.makedirs(os.path.join(base, 'cps/{}'.format(exp_name)))
     
-elif model_name == 'EfficientNet-B2':
-    efficient = EfficientNet(nih_weight)
-    base = efficient.buildTunerModel(img_size)
-    model = efficient.buildBaseModel(img_size)
-    model = efficient.freeze(model)
-    efficient.compileModel(model, lr, momentum, nestrov)
+    if not os.path.exists(base + 'best_models_and_params/{}/model'.format(exp_name)):
+        os.makedirs(base + 'best_models_and_params/{}/model'.format(exp_name))
+    
+    return train_path, valid_path, freeze_save_path, unfreeze_save_path, best_model_path, best_weight_path, best_param_path
 
-patience_rlr = 3
-patience_es = 5
-factor = 0.1
-min_delta = 0.001
-monitor = 'val_auc'
-epoch = 50
+def get_model(model_name):
+    # Train with only pooling layers
+    if model_name == 'ResNet-50':
+        resnet = ResNet(nih_weight)
+        base = resnet.buildTunerModel(img_size)
+        model = resnet.buildBaseModel(img_size)
+        model = resnet.freeze(model)
+        resnet.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Xception':
+        xception = XceptionNet(nih_weight)
+        base = xception.buildTunerModel(img_size)
+        model = xception.buildBaseModel(img_size)
+        model = xception.freeze(model)
+        xception.compileModel(model, lr, momentum, nestrov)
+    
+    elif model_name == 'DenseNet-121':
+        dense = DenseNet(nih_weight)
+        base = dense.buildTunerModel(img_size)
+        model = dense.buildBaseModel(img_size)
+        model = dense.freeze(model)
+        dense.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Inception-V3':
+        inception = InceptionNet(nih_weight)
+        base = inception.buildTunerModel(img_size)
+        model = inception.buildBaseModel(img_size)
+        model = inception.freeze(model)
+        inception.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'Inception-ResNet-V2':
+        inceptionres = InceptionResNet(nih_weight)
+        base = inceptionres.buildTunerModel(img_size)
+        model = inceptionres.buildBaseModel(img_size)
+        model = inceptionres.freeze(model)
+        inceptionres.compileModel(model, lr, momentum, nestrov)
+        
+    elif model_name == 'EfficientNet-B2':
+        efficient = EfficientNet(nih_weight)
+        base = efficient.buildTunerModel(img_size)
+        model = efficient.buildBaseModel(img_size)
+        model = efficient.freeze(model)
+        efficient.compileModel(model, lr, momentum, nestrov)
+        
+    return model, base
 
-features = trainFeatures()
-rlr = features.setRLP(monitor, factor, patience_rlr)
-es = features.setES(monitor, patience_es, min_delta)
-cp = features.setCP(monitor, freeze_save_path)
 
-model_history = features.generator(model, train_gen, val_gen, epoch, cp, rlr, es)
-img_proc.plot_save(model_history, base_folder, exp_name)
+        
+if __name__=='__main__':
+    
+    batch_size = 16
+    rotation_range = 20
+    height_shift = 0.05
+    width_shift = 0.05
+    
+    args = get_args()
 
-# Add dropout layer and run tuner with entire model
+    data_path = args.path[0]
+    model_name = args.model_name
+    img_size = args.img_size
+    
+    nih_weight = 'nih_weights_{name}.h5'.format(name = model_name)
 
-model = features.load(model, freeze_save_path)
-model = features.unfreeze(model)
+    if not os.path.exists(nih_weight):
+        print('NIH weight does not exists.'
+              ' Please provide a NIH weight file in the format of nih_weight_[model name].h5')
+        exit()
+    
+    exp_name = 'tuner_pretrain'
+    base_dir = os.getcwd()
+    
+    train_dir, valid_dir, freeze_dir, unfreeze_dir, model_dir, weight_dir, param_dir = make_path(data_path, base_dir, exp_name)
 
-patience_es = 10
+    img_proc = imgUtils(img_size)
+    train_idg, val_idg = img_proc.dataGen(rotation_range, height_shift, width_shift)
+    train_gen, val_gen = img_proc.generator(batch_size, train_idg, val_idg, train_dir, valid_dir)
 
-es = features.setES(monitor, patience_es, min_delta)
-cp = features.setCP(monitor, unfreeze_save_path)
+    lr = 0.001
+    momentum = 0.9
+    nestrov = True
+    patience_rlr = 3
+    patience_es = 5
+    factor = 0.1
+    min_delta = 0.001
+    monitor = 'val_auc'
+    epoch = 50
 
-hp = hyperModel(base, freeze_save_path)
+    features = trainFeatures()
+    rlr = features.setRLP(monitor, factor, patience_rlr)
+    es = features.setES(monitor, patience_es, min_delta)
+    cp = features.setCP(monitor, freeze_dir)
+    
+    model, base = get_model(model_name)
 
-TOTAL_TRIALS = 10
-EXECUTION_PER_TRIAL = 1
-EPOCHS = 50
+    model_history = features.generator(model, train_gen, val_gen, epoch, cp, rlr, es)
+    img_proc.plot_save(model_history, base_dir, exp_name)
 
-tuner = BayesianOptimization(
-    hp,
-    max_trials=TOTAL_TRIALS,
-    objective=kerastuner.Objective("val_auc", direction="max"),
-    executions_per_trial=EXECUTION_PER_TRIAL,
-    directory='bayesian',
-    project_name=exp_name
-)
+    # Add dropout layer and run tuner with entire model
+    
+    model = features.load(model, freeze_dir)
+    model = features.unfreeze(model)
 
-history = tuner.search(train_gen, 
-                       epochs=EPOCHS,
-                       validation_data=val_gen, 
-                        callbacks = [es, cp],
-                        verbose =2,
-                        use_multiprocessing=False)
+    patience_es = 10
+    
+    es = features.setES(monitor, patience_es, min_delta)
+    cp = features.setCP(monitor, unfreeze_dir)
 
-# Save best model and weight
-best_model = tuner.get_best_models(num_models=TOTAL_TRIALS)[0]
-best_config = best_model.optimizer.get_config()
+    hp = hyperModel(base, freeze_dir)
 
-hyperparameters = tuner.get_best_hyperparameters(num_trials=TOTAL_TRIALS)
-best_hyperparameters = hyperparameters[0].get_config()
+    TOTAL_TRIALS = 10
+    EXECUTION_PER_TRIAL = 1
+    EPOCHS = 50
 
-model.save(best_model_path)
-model.save_weights(best_weight_path)
-with open(best_param_path, "w") as text_file:
-    text_file.write(str(best_hyperparameters))
+    tuner = BayesianOptimization(
+        hp,
+        max_trials=TOTAL_TRIALS,
+        objective=kerastuner.Objective("val_auc", direction="max"),
+        executions_per_trial=EXECUTION_PER_TRIAL,
+        directory='bayesian',
+        project_name=exp_name
+    )
+
+    history = tuner.search(train_gen, 
+                           epochs=EPOCHS,
+                           validation_data=val_gen, 
+                            callbacks = [es, cp],
+                            verbose =2,
+                            use_multiprocessing=False)
+
+    # Save best model and weight
+    best_model = tuner.get_best_models(num_models=TOTAL_TRIALS)[0]
+    best_config = best_model.optimizer.get_config()
+    
+    hyperparameters = tuner.get_best_hyperparameters(num_trials=TOTAL_TRIALS)
+    best_hyperparameters = hyperparameters[0].get_config()
+    
+    model.save(model_dir)
+    model.save_weights(weight_dir)
+    with open(param_dir, "w") as text_file:
+        text_file.write(str(best_hyperparameters))
 
 
 
