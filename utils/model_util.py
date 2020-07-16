8e641fd2ebd12856b99f477bbb55db09dc067bc0
jsheng7 <39497817+jsheng7@users.noreply.github.com>
2020-07-15 21:26:31 -0500

    Improved some functions

--- utils/model_util.py
+++ utils/model_util.py
@@ -1,81 +1,110 @@
 # Set checkpoint, early stopping and reduce learning rate on plataeu
+import os
 from tensorflow.keras import optimizers
 from keras.callbacks.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from covid_models import hyperModel, DenseNet, ResNet, XceptionNet, EfficientNet, InceptionNet, InceptionResNet 
+from covid_models import DenseNet, ResNet, XceptionNet, EfficientNet, InceptionNet, InceptionResNet 
 
 class trainFeatures():
-    def getModel(self, model_name):
+    def getModel(self, model_name, img_size, weight):
         if model_name == 'ResNet-50':
-            resnet = ResNet(nih_weight)
+            resnet = ResNet(weight)
             base = resnet.buildTunerModel(img_size)
             model = resnet.buildBaseModel(img_size)
             freeze_model = resnet.freeze(model)
 
         elif model_name == 'Xception':
-            xception = XceptionNet(nih_weight)
+            xception = XceptionNet(weight)
             base = xception.buildTunerModel(img_size)
             model = xception.buildBaseModel(img_size)
             freeze_model = xception.freeze(model)
 
         elif model_name == 'DenseNet-121':
-            dense = DenseNet(nih_weight)
+            dense = DenseNet(weight)
             base = dense.buildTunerModel(img_size)
             model = dense.buildBaseModel(img_size)
             freeze_model = dense.freeze(model)
 
         elif model_name == 'Inception-V3':
-            inception = InceptionNet(nih_weight)
+            inception = InceptionNet(weight)
             base = inception.buildTunerModel(img_size)
             model = inception.buildBaseModel(img_size)
             freeze_model = inception.freeze(model)
 
         elif model_name == 'Inception-ResNet-V2':
-            inceptionres = InceptionResNet(nih_weight)
+            inceptionres = InceptionResNet(weight)
             base = inceptionres.buildTunerModel(img_size)
             model = inceptionres.buildBaseModel(img_size)
             freeze_model = inceptionres.freeze(model)
 
         elif model_name == 'EfficientNet-B2':
-            efficient = EfficientNet(nih_weight)
+            efficient = EfficientNet(weight)
             base = efficient.buildTunerModel(img_size)
             model = efficient.buildBaseModel(img_size)
             model = efficient.freeze(model)
 
         return freeze_model, model, base
     
-    def getDropoutModel(self, model_name, dropout):
-        # Train with only pooling layers
+    def getDropoutModel(self, model_name, weight, dropout):
         if model_name == 'ResNet-50':
-            resnet = ResNet(nih_weight)
+            resnet = ResNet(weight)
             drop_model = resnet.buildDropModel(dropout)
             drop_model = resnet.freeze(drop_model)
 
         elif model_name == 'Xception':
-            xception = XceptionNet(nih_weight)
+            xception = XceptionNet(weight)
             drop_model = xception.buildDropModel(dropout)
             drop_model = xception.freeze(drop_model)
 
         elif model_name == 'DenseNet-121':
-            dense = DenseNet(nih_weight)
+            dense = DenseNet(weight)
             drop_model = dense.buildDropModel(dropout)
             drop_model = dense.freeze(drop_model)
 
         elif model_name == 'Inception-V3':
-            inception = InceptionNet(nih_weight)
+            inception = InceptionNet(weight)
             drop_model = inception.buildDropModel(dropout)
             drop_model = inception.freeze(drop_model)
 
         elif model_name == 'Inception-ResNet-V2':
-            inceptionres = InceptionResNet(nih_weight)
+            inceptionres = InceptionResNet(weight)
             drop_model = inceptionres.buildDropModel(dropout)
             drop_model = inceptionres.freeze(drop_model)
 
         elif model_name == 'EfficientNet-B2':
-            efficient = EfficientNet(nih_weight)
+            efficient = EfficientNet(weight)
             drop_model = efficient.buildDropModel(dropout)
             drop_model = efficient.freeze(drop_model)
 
         return drop_model
+    
+    def getAllModel(self, img_size, weight_dir, crop_stat):
+        res_weight = os.path.join(weight_dir, 
+                                  'ResNet50_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        xception_weight = os.path.join(weight_dir,
+                                       'Xception_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        dense_weight = os.path.join(weight_dir,
+                                    'DenseNet_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        inception_weight = os.path.join(weight_dir, 
+                                        'Inception_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        inceptionres_weight = os.path.join(weight_dir, 
+                                           'InceptionResNet_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        efficient_weight = os.path.join(weight_dir, 
+                                        'EfficientNet_{size}_up_{crop}.h5'.format(size = img_size, crop = crop_stat))
+        
+        resnet = ResNet(res_weight)
+        res_model = resnet.buildBaseModel(img_size)
+        xception = XceptionNet(xception_weight)
+        xception_model = xception.buildBaseModel(img_size)
+        dense = DenseNet(dense_weight)
+        dense_model = dense.buildBaseModel(img_size)
+        inception = InceptionNet(inception_weight)
+        inception_model = inception.buildBaseModel(img_size)
+        inceptionres = InceptionResNet(inceptionres_weight)
+        inceptionres_model = inceptionres.buildBaseModel(img_size)
+        efficient = EfficientNet(efficient_weight)
+        efficient_model = efficient.buildBaseModel(img_size)
+        
+        return res_model, xception_model, dense_model, inception_model, inceptionres_model, efficient_model
 
     def setCP(self, monitor, model_path):
         checkpoint = ModelCheckpoint(
