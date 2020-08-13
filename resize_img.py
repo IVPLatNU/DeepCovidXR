efc2b2c4b8e9a9484a9a0debcc797eeab46c73fa
YunanWu <yunanwu2020@u.northwestern.edu>
2020-08-13 17:20:44 -0500

    Add files via upload

---
+++ resize_img.py
@@ -0,0 +1,48 @@
+import glob
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import os, shutil
+import pandas as pd
+import csv
+import nibabel as nib
+import skimage
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--input_path', type = str, help = 'the directory of the input path')
+parser.add_argument('-o','--output_path', type = str, help = 'the directory of the output path')
+parser.add_argument('-s', '--size', type = int, help = 'the cropping size')
+args = parser.parse_args()
+
+input_path = args.input_path
+output_path = args.output_path
+img_size = args.size
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+
+LFHTest = []
+for file in glob.glob(input_path + '/*'):
+#     print(file)
+    path, filename = os.path.split(file)
+#     print(file)
+    filename2, extension = filename.rsplit('.', 1)
+    LFHTest.append(filename2)
+Test_set = set(LFHTest)
+matches = []
+for file in glob.glob(input_path + '/*'):
+    path, filename = os.path.split(file)
+    
+    #no crop
+    filename2, junk2 = filename.rsplit('.', 2)
+    #crop
+    if filename2 in Test_set:
+        im = Image.open(file)
+        im = im.resize((img_size,img_size), resample=Image.LANCZOS)
+        im.save(os.path.join(output_path,filename))
+        matches.append(filename)
+print('Successfully resize {} images to {}*{}'.format( len(matches), img_size, img_size))
