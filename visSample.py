8a8edba7d23556e94e3a7f6c31289c7b26eba1f4
jsheng7 <jiayuesheng2021@u.northwestern.edu>
2020-08-14 00:50:48 -0500

    Fixed grad-CAM color issue

--- visSample.py
+++ visSample.py
@@ -3,12 +3,14 @@ import numpy as np
 import os
 from tensorflow.keras.preprocessing import image
 import argparse
-
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from vis.visualization import  overlay
 from utils import imgUtils, trainFeatures
 
 def get_args():
     # Implement command line argument
-    parser = argparse.ArgumentParser(description='For each input image, generates COVID possibilities for 224x224 and 331x331 versions.')
+    parser = argparse.ArgumentParser(description='For each input image, generates and saves a grad-CAM image.')
     parser.add_argument('--weight', '-w', dest='weight_path', metavar='weight_path', 
                         type=str, nargs=1,
                         required = True, help='the path that contains trained weights.')
@@ -18,7 +20,7 @@ def get_args():
 if __name__=='__main__':
     args = get_args()
     weights = args.weight_path[0]
-#    weights = 'D:\covid\Ensemble\covid_weights'
+    #weights = 'D:\covid\Ensemble\covid_weights'
     img_size = 224
     dropout = 0.3
     crop_stat = 'uncrop'
@@ -35,11 +37,28 @@ if __name__=='__main__':
     img_preproc = img_proc.preprocess(img_array)
     img = np.expand_dims(img_preproc, axis=0)
 
-    res_224, xception_224, dense_224, inception_224, inceptionres_224, efficient_224 = features.getAllDropModel(img_size, weights, crop_stat, dropout)
+    res_224, xception_224, dense_224, inception_224, inceptionres_224, efficient_224 = features.getAllModel(img_size, weights, crop_stat)
     models = [res_224, xception_224, dense_224, inception_224, inceptionres_224, efficient_224]
-    
+    result_name = 'sample1_gradcam.jpg'
+    result_path = os.path.join(os.getcwd(),'sample_images', result_name)
     # Save overlay heatmap
-    visualization = img_proc.gradCAM(img, img_preproc, models)
+    visualization_avg = img_proc.gradCAM(img, img_preproc, models)
+    
+    plt.rcParams['figure.figsize'] = (18, 6)
+
+    # Matplotlib preparations
+    fig, axes = plt.subplots(1, 3)
+    
+    axes[0].imshow(img_array[..., 0], cmap='gray') 
+    axes[0].set_title('Input')
+    
+    heatmap_avg = np.uint8(visualization_avg)
+    original = np.uint8(cm.gray(img_array[..., 0])[..., :3]*255)
+    axes[1].imshow(heatmap_avg)
+    axes[1].set_title('Grad-CAM')
+    axes[2].imshow(overlay(heatmap_avg, original))
+    axes[2].set_title('Overlay')
+    plt.show()
 
 
 
