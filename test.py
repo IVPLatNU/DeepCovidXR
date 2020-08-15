ada57a4568df664d4016953641ce092a43c6476c
jsheng7 <jiayuesheng2021@u.northwestern.edu>
2020-08-14 22:02:13 -0500

    Changed the comination of predictions

--- test.py
+++ test.py
@@ -24,6 +24,11 @@ def get_args():
                         required = True, help='the path to the image.')
     
     return parser.parse_args()
+
+# Resize image to 224 and 331
+def img_resize(img, img_size):
+    i = 0
+    
     
 
 def get_model_list(weight_path):
@@ -64,7 +69,29 @@ def get_model_list(weight_path):
         
     return model_list
 
-def test_individual(model_list, pickle_path, input_img_224, input_img_331):
+def get_pred(input_img, model_list, weight_list):
+    tta_steps = 5
+    combined_weighted_probs = []
+    combined_probs = []
+    
+    for model, weight in zip(model_list, weight_list):
+        predictions = []
+        for i in tqdm(range(tta_steps)):
+            preds = model.predict(input_img, verbose=1)
+            predictions.append(preds)
+        Y_pred = np.mean(predictions, axis=0)
+        y_pred = np.multiply(Y_pred, weight)
+        combined_probs.append(Y_pred)
+        combined_weighted_probs.append(y_pred)
+        
+    combined_probs = np.asarray(combined_probs)
+    combined_weighted_probs = np.asarray(combined_weighted_probs)
+    
+    ensemble_pred = np.sum(combined_weighted_probs)
+    return ensemble_pred
+
+def test_individual(model_list, pickle_path, input_img_224, input_img_224_crop, 
+                    input_img_331, input_img_331_crop):
     ensemble_weights = pickle.load(open(pickle_path, "rb"))
 
     model_list_224_uncrop = model_list[0]+model_list[4]+model_list[8]+model_list[12]+model_list[16]+model_list[20]
@@ -75,45 +102,53 @@ def test_individual(model_list, pickle_path, input_img_224, input_img_331):
     ensemble_weights_224_crop = ensemble_weights[1]+ensemble_weights[5]+ensemble_weights[9]+ensemble_weights[13]+ensemble_weights[17]+ensemble_weights[21]
     ensemble_weights_331_uncrop = ensemble_weights[2]+ensemble_weights[6]+ensemble_weights[10]+ensemble_weights[14]+ensemble_weights[18]+ensemble_weights[22]
     ensemble_weights_331_crop = ensemble_weights[3]+ensemble_weights[7]+ensemble_weights[11]+ensemble_weights[15]+ensemble_weights[19]+ensemble_weights[23]
-    #ensemble_weights = ensemble_weights_224 + ensemble_weights_331
-    tta_steps = 5
-    combined_weighted_probs = []
-    combined_probs = []
     
-    for model, weight in zip(model_list, ensemble_weights):
-        predictions = []
-        for i in tqdm(range(tta_steps)):
-            preds = model.predict(input_img_224, verbose=1)
-            predictions.append(preds)
-        Y_pred = np.mean(predictions, axis=0)
-        y_pred = np.multiply(Y_pred, weight)
-        combined_probs.append(Y_pred)
-        combined_weighted_probs.append(y_pred)
-        
-    combined_probs = np.asarray(combined_probs)
-    combined_weighted_probs = np.asarray(combined_weighted_probs)
+    pred_224_uncrop = get_pred(input_img_224, model_list_224_uncrop, ensemble_weights_224_uncrop)
+    pred_331_uncrop = get_pred(input_img_331, model_list_331_uncrop, ensemble_weights_331_uncrop)
+    pred_224_crop = get_pred(input_img_224_crop, model_list_224_crop, ensemble_weights_224_crop)
+    pred_331_crop = get_pred(input_img_331_crop, model_list_331_crop, ensemble_weights_331_crop)
     
-    ensemble_pred_224 = np.sum(combined_weighted_probs)
+    ensemble_pred = np.mean(pred_224_uncrop, pred_224_crop, pred_331_uncrop, pred_331_crop)
     
-    combined_probs = []
-    combined_weighted_probs = []
-        
-    for model, weight in zip(model_list, ensemble_weights):
-        predictions = []
-        for i in tqdm(range(tta_steps)):
-            preds = model.predict(input_img_331, verbose=1)
-            predictions.append(preds)
-        Y_pred = np.mean(predictions, axis=0)
-        y_pred = np.multiply(Y_pred, weight)
-        combined_probs.append(Y_pred)
-        combined_weighted_probs.append(y_pred)
-    
-    combined_probs = np.asarray(combined_probs)
-    combined_weighted_probs = np.asarray(combined_weighted_probs)
-    
-    ensemble_pred_331 = np.sum(combined_weighted_probs)
-    
-    return ensemble_pred_224, ensemble_pred_331
+    return ensemble_pred
+# =============================================================================
+#     tta_steps = 5
+#     combined_weighted_probs = []
+#     combined_probs = []
+#     
+#     for model, weight in zip(model_list_224_uncrop, ensemble_weights_224_uncrop):
+#         predictions = []
+#         for i in tqdm(range(tta_steps)):
+#             preds = model.predict(input_img_224, verbose=1)
+#             predictions.append(preds)
+#         Y_pred = np.mean(predictions, axis=0)
+#         y_pred = np.multiply(Y_pred, weight)
+#         combined_probs.append(Y_pred)
+#         combined_weighted_probs.append(y_pred)
+#         
+#     combined_probs = np.asarray(combined_probs)
+#     combined_weighted_probs = np.asarray(combined_weighted_probs)
+#     
+#     ensemble_pred_224_uncrop = np.sum(combined_weighted_probs)
+#     
+#     combined_probs = []
+#     combined_weighted_probs = []
+#         
+#     for model, weight in zip(model_list_331_uncrop, ensemble_weights_331_uncrop):
+#         predictions = []
+#         for i in tqdm(range(tta_steps)):
+#             preds = model.predict(input_img_331, verbose=1)
+#             predictions.append(preds)
+#         Y_pred = np.mean(predictions, axis=0)
+#         y_pred = np.multiply(Y_pred, weight)
+#         combined_probs.append(Y_pred)
+#         combined_weighted_probs.append(y_pred)
+#     
+#     combined_probs = np.asarray(combined_probs)
+#     combined_weighted_probs = np.asarray(combined_weighted_probs)
+#     
+#     ensemble_pred_331_uncrop = np.sum(combined_weighted_probs)
+# =============================================================================
 
 if __name__=='__main__':
     args = get_args()
